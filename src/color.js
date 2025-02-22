eac20222e10e5d36e37c4e3840e34aec70c2096a
nousantx <nousantx@gmail.com>
2025-02-22 12:03:45 +0700

    feat: update exports

--- src/color.js
+++ src/color.js
@@ -1,38 +1,68 @@
-export const colors = {
-  primary: '#ccf654',
-  red: '#f03838',
-  green: '#15e05f',
-  blue: '#3d82f2',
-  yellow: '#f1c230',
-  slate: '#636c7c',
-  neutral: '#737373'
+// Main colors
+export const primary = '#ccf654'
+export const red = '#f03838'
+export const green = '#15e05f'
+export const blue = '#3d82f2'
+export const yellow = '#f1c230'
+export const slate = '#636c7c'
+export const neutral = '#737373'
+
+// Extended colors
+export const ruby = '#e0115f'
+export const tomato = '#ff6347'
+export const salmon = '#fa8072'
+export const orange = '#f37f2e'
+export const amber = '#f49c09'
+export const gold = '#ffd700'
+export const chartreuse = '#7fff00'
+export const lime = '#91e411'
+export const emerald = '#13cb8e'
+export const teal = '#12c39f'
+export const aquamarine = '#7fffd4'
+export const turquoise = '#40e0d0'
+export const cyan = '#21d2f1'
+export const sky = '#0ea5e9'
+export const indigo = '#6f72f7'
+export const violet = '#7844f0'
+export const purple = '#a855f7'
+export const magenta = '#ff00ff'
+export const fuchsia = '#d642ec'
+export const pink = '#f32d8f'
+export const rose = '#eb3756'
+export const zinc = '#71757a'
+export const gray = '#696a6a'
+
+const colors = {
+  primary,
+  red,
+  green,
+  blue,
+  yellow,
+  slate,
+  neutral,
+  ruby,
+  tomato,
+  salmon,
+  orange,
+  amber,
+  gold,
+  chartreuse,
+  lime,
+  emerald,
+  teal,
+  aquamarine,
+  turquoise,
+  cyan,
+  sky,
+  indigo,
+  violet,
+  purple,
+  magenta,
+  fuchsia,
+  pink,
+  rose,
+  zinc,
+  gray
 }
 
-export const allColors = {
-  ...colors,
-  ruby: '#e0115f',
-  tomato: '#ff6347',
-  salmon: '#fa8072',
-  orange: '#f37f2e',
-  amber: '#f49c09',
-  gold: '#ffd700',
-  chartreuse: '#7fff00',
-  lime: '#91e411',
-  emerald: '#13cb8e',
-  teal: '#12c39f',
-  aquamarine: '#7fffd4',
-  turquoise: '#40e0d0',
-  cyan: '#21d2f1',
-  sky: '#0ea5e9',
-  indigo: '#6f72f7',
-  violet: '#7844f0',
-  purple: '#a855f7',
-  magenta: '#ff00ff',
-  fuchsia: '#d642ec',
-  pink: '#f32d8f',
-  rose: '#eb3756',
-  zinc: '#71757a',
-  gray: '#696a6a'
-}
-
-export default allColors
+export default colors
